328e78cd00324aa7284a0af2d29f02297132c9d9
Baguspanji <pitungs060@gmail.com>
2021-09-08 10:26:56 +0700

    profil page

---
+++ lib/home.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/homePage.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF838C8A),
+        title: Text(
+          'Profil Page',
+          style: TextStyle(fontSize: 30),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(80),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1600721391735-65e89787737f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'),
+                    ),
+                  ),
+                ),
+              ),
+              buildItems('Jasmine Crew'),
+              buildItems('Age 21'),
+              buildItems('May 29 1999'),
+              buildItems('New York, USA'),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF838C8A)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ));
+                  },
+                  child: Text('Store Page'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildItems(String title) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(14),
+        side: BorderSide(color: Colors.grey, width: 2),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: EdgeInsets.all(6),
+        child: ListTile(
+          title: Text(
+            title,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
 import 'package:flutter_application_1/homePage.dart';
 
 void main() {
@@ -12,94 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Belajar',
-          style: TextStyle(fontSize: 30),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: ListView(
-            children: [
-              buildItems('Muhammad Bagus Panji', 'Pasuruan, 18 Mei 1999',
-                  'Age 22', 'Tosari Pasuruan'),
-              buildItems('Alvin', 'Pasuruan, 22 Agustus 1992', 'Age 29',
-                  'Rembang Pasuruan'),
-              buildItems('Wildan', 'Pasuruan, 22 Juni 1992', 'Age 29',
-                  'Rembang Pasuruan'),
-            ],
-          ),
-        ),
-      ),
-      bottomSheet: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 60,
-        padding: EdgeInsets.symmetric(horizontal: 6),
-        child: ElevatedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(30),
-                ),
-              ),
-            ),
-          ),
-          child: Text(
-            'Button',
-            style: TextStyle(fontSize: 30),
-          ),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HomePage(),
-                ));
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget buildItems(String nama, String ttl, String age, String alamat) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(14),
-        side: BorderSide(color: Colors.grey, width: 2),
-      ),
-      elevation: 4,
-      child: Padding(
-        padding: EdgeInsets.all(6),
-        child: ListTile(
-          leading: Icon(
-            Icons.person,
-            size: 40,
-            color: Colors.blue,
-          ),
-          title: Text(nama),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(ttl),
-              Text(age),
-              Text(alamat),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
